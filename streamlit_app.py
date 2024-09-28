9b2a4b356dbe3562ba99d77704deab24c7859b4b
Prashant <prashant.patel77@gmail.com>
2024-09-28 10:45:11 +0000

    Comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
 
+    # TODO: use of local LLM
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
